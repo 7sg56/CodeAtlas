23312eaa9d6ed7923089e30a9cf2bea2acd37c73
Sourish Ghosh <sghosh.ile.7@gmail.com>
2026-02-27 23:59:12 +0530

    Updated docker-compose

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ app.use(express.json());
 
 app.use("/api/analyze", analyzeRoute);
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.BACKEND_PORT || process.env.PORT || 5001;
 
 app.listen(PORT, async () => {
   console.log(`Server running on port ${PORT}`);
